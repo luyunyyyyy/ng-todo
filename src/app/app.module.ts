5098a615e49c0c0f65a3589ee8421664dfffb427
Luyunyyyyy <luyunyang1@foxmail.com>
2019-01-29 11:31:25 +0800

    change title and init input-todo-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputTodoItemComponent } from './input-todo-item/input-todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputTodoItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/input-todo-item/input-todo-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-todo-item',
+  templateUrl: './input-todo-item.component.html',
+  styleUrls: ['./input-todo-item.component.css']
+})
+export class InputTodoItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
